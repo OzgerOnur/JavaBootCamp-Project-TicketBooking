4ea3a1b4ca534dfd6338699d3f64a51041bc4013
Onur Ozger <n.onur@yandex.com>
2021-11-04 22:52:59 +0300

    starting to create models

---
+++ src/main/java/com/kodluyoruz/flightticket/model/Passenger.java
@@ -0,0 +1,32 @@
+package com.kodluyoruz.flightticket.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "passengers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Passenger extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Integer id;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy = "passenger",orphanRemoval = true)
+    private List<Ticket> ticket;
+
+
+    private String name;
+
+
+
+
+
+}
---
+++ src/main/java/com/kodluyoruz/flightticket/model/Ticket.java
@@ -0,0 +1,32 @@
+package com.kodluyoruz.flightticket.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Ticket extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Integer id;
+
+    @Column(name = "passenger_id")
+    private Integer passengerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passenger_id",updatable = false,insertable = false,nullable = false)
+    private Passenger passenger;
+
+    @OneToOne(fetch = FetchType.LAZY,orphanRemoval = true,cascade = CascadeType.ALL)
+    private Seat seat;
+
+
+
+}
